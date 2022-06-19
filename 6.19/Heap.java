2a54c6cda3bf11625cbc6d7537d1a7d5c2e77673
Ryanwjr <1046461877@qq.com>
2022-06-19 15:15:09 +0800

    Create Heap.java

---
+++ 6.19/Heap.java
@@ -0,0 +1,48 @@
+
+//堆排序-完全二叉树-原地 Nlogn-每个节点大等或小等子树中的每个节点-堆顶极值
+public class Heap {
+    private int[] a; // 数组，从下标1开始存储数据
+    private int n; // 堆可以存储的最大数据个数
+    private int count; // 堆中已经存储的数据个数
+
+    public Heap(int capacity) {
+        a = new int[capacity + 1];
+        n = capacity;
+        count = 0;
+    }
+
+    public void insert(int data) {
+        if (count >= n)
+            return; // 堆满了
+        ++count;
+        a[count] = data;
+        int i = count;
+        while (i / 2 > 0 && a[i] > a[i / 2]) { // 自下往上堆化
+            swap(a, i, i / 2); // swap()函数作用：交换下标为i和i/2的两个元素
+            i = i / 2;
+        }
+    }
+
+    public void removeMax() {
+        if (count == 0)
+            return -1; // 堆中没有数据
+        a[1] = a[count];
+        --count;
+        heapify(a, count, 1);
+    }
+
+    private void heapify(int[] a, int n, int i) { // 自上往下堆化
+        while (true) {
+            int maxPos = i;
+            if (i * 2 <= n && a[i] < a[i * 2])
+                maxPos = i * 2;
+            if (i * 2 + 1 <= n && a[maxPos] < a[i * 2 + 1])
+                maxPos = i * 2 + 1;
+            if (maxPos == i)
+                break;
+            swap(a, i, maxPos);
+            i = maxPos;
+        }
+    }
+
+}
