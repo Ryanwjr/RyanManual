8f255eb65cb93ae765e16928df840392fc5dd5b1
Ryanwjr <1046461877@qq.com>
2022-06-28 11:37:31 +0800

    Create Tree.cpp

---
+++ 6.28/Tree.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+// find the common ancestor node
+struct Tree
+{
+    int data = [ 1, -1, 2, 3, 4, 5, 6, 7 ];
+};
+
+int Common(Tree t, int i, int j)
+{
+    if (t.data[i] != -1 && t.data[j] != -1)
+    {
+        while (i != j)
+        {
+            if (i > j)
+                i /= 2;
+            else
+                j /= 2;
+        }
+        return t.data[i];
+    }
+    return -1;
+}
+int main()
+{
+    Tree t;
+    int ans = Common(t, 3, 6);
+    return 0;
+}
