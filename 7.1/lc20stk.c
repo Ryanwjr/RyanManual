23547bc09ddb0c057ba725531d254432093f6a1a
Ryanwjr <1046461877@qq.com>
2022-07-01 10:15:59 +0800

    c

---
+++ 7.1/lc20stk.c
@@ -0,0 +1,37 @@
+char pair(char a)
+{
+    if (a == '}')
+        return '{';
+    if (a == ']')
+        return '[';
+    if (a == ')')
+        return '(';
+    return 0;
+}
+bool isValid(char *s)
+{
+    int n = strlen(s);
+    if (n % 2 == 1)
+        return false;
+    else
+    {
+        int stk[n + 1], top = 0;
+        for (int i = 0; i < n; i++)
+        {
+            char pa = pair(s[i]);
+            if (pa)
+            {
+                if (top == 0 || stk[top - 1] != pa)
+                {
+                    return false;
+                }
+                top--;
+            }
+            else
+            {
+                stk[top++] = s[i];
+            }
+        }
+        return top == 0;
+    }
+}
