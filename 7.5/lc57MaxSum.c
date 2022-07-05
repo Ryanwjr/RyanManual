0df29173dc4440fe323fc9ad888826157c68d4f7
Ryanwjr <1046461877@qq.com>
2022-07-05 20:04:11 +0800

    Create lc57MaxSum.c

---
+++ 7.5/lc57MaxSum.c
@@ -0,0 +1,11 @@
+int maxSubArray(int *nums, int numsSize)
+{
+    int pre = 0;
+    int max = nums[0];
+    for (int i = 0; i < numsSize; i++)
+    {
+        pre = fmax(pre + nums[i], nums[i]);
+        max = fmax(max, pre);
+    }
+    return max;
+}
